02c371066c8a33a3e29ca56e75f678684a6f2f69
lupyuen-unabiz <ly.lee@unabiz.comly.lee@unabiz.com>
2017-12-17 14:03:52 +0800

    Adding sample for finite state machine

---
+++ test/unabiz-arduino.ino
@@ -0,0 +1,12 @@
+//  The demo sketch has been moved to
+//  https://github.com/UnaBiz/unabiz-arduino/blob/master/examples/DemoTest/DemoTest.ino
+
+#ifdef CLION
+//  Test the build of each example with CLion.
+//#include "examples/DemoTest/DemoTest.ino"
+//#include "examples/send-test/send-test.ino"
+//#include "examples/read-temperature/read-temperature.ino"
+//#include "examples/send-temperature/send-temperature.ino"
+//#include "examples/read-light-level/read-light-level.ino"
+#include "examples/send-light-level/send-light-level.ino"
+#endif  //  CLION
