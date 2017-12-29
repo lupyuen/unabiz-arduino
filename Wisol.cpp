aaf7e58e4b977b95623787db4194d0688e424b06
lupyuen-unabiz <ly.lee@unabiz.comly.lee@unabiz.com>
2017-12-29 17:44:40 +0800

    Refactor sendBuffer into FSM

--- Wisol.cpp
+++ Wisol.cpp
@@ -66,33 +66,82 @@ static const uint8_t stepReceive = 4;
 static const uint8_t stepTimeout = 5;
 static const uint8_t stepEnd = 6;
 
+static const uint8_t stepSuccess = 101;
+static const uint8_t stepFailure = 102;
+
 static const uint16_t delayAfterStart = 200;
 static const uint16_t delayAfterSend = 10;
+
+struct FunctionState {
+  //  Represents the state of a function.  Remembers the current step and the success/failure status.
+  String functionName = "";
+  FunctionState *parentState = 0;
+  FunctionState *childState = 0;
+  uint8_t currentStep = 0;
+  uint8_t nextStep = 0;
+  int int1 = 0;
+  unsigned long ulong1 = 0;
+};
 
 class State {
 public:
   uint8_t begin(const String &functionName, uint8_t firstStep) {
-    return step;
+    //  If we don't have a child state for this function name, create a new state and reset to the first step.
+    FunctionState *childState = currentState->childState;
+    if (!childState || childState->functionName != functionName) {
+      log2(F("New state"), functionName);
+      if (childState) delete childState;
+      childState = new FunctionState();
+      childState->functionName = functionName;
+      childState->currentStep = firstStep;
+      childState->parentState = currentState;
+      currentState->childState = childState;
+      currentState = childState;
+    } else {
+      //  Else switch to the state and return the last step for this function.
+      log2(F("Found state"), functionName);
+      currentState = childState;
+    }
+    return currentState->currentStep;
   }
+
+  void popState(uint8_t currentStep0, uint8_t nextStep0 = 0) {
+    if (!currentState) return;
+    currentState->currentStep = currentStep0;
+    currentState->nextStep = nextStep0;
+    currentState = currentState->parentState;
+    log2(F("Pop state"), currentState->functionName);
+  }
+
   bool end() {
-    //  TODO
+    //  The function has finished with a success status.
+    popState(stepSuccess);
     return true;
   }
+
   bool endWithFailure() {
-    //  TODO
+    //  The function has finished with a failure status.
+    popState(stepFailure);
     return false;
   }
-  bool suspend() { return suspend(step); }
-  bool suspend(uint8_t step0, uint32_t delay = 0) { step = step0; return true; }
-  void setState(int int1, unsigned long ulong1) { state.int1 = int1; state.ulong1 = ulong1; }
-  void getState(int &int1, unsigned long &ulong1) { int1 = state.int1; ulong1 = state.ulong1; }
+
+  bool suspend() {
+    //  Pop the current state.
+    return suspend(currentState->nextStep);
+  }
+
+  bool suspend(uint8_t nextStep, uint32_t delay = 0) {
+    //  Remember the next step and pop the current state.
+    popState(currentState->currentStep, nextStep);
+    //  TODO: Delay
+    return true;
+  }
+
+  void setState(int int1, unsigned long ulong1) { currentState->int1 = int1; currentState->ulong1 = ulong1; }
+  void getState(int &int1, unsigned long &ulong1) { int1 = currentState->int1; ulong1 = currentState->ulong1; }
 
 private:
-  uint8_t step = 0;
-  struct {
-    int int1 = 0;
-    unsigned long ulong1 = 0;
-  } state;
+  FunctionState *currentState = 0;
 };
 
 bool Wisol::sendBuffer(const String &buffer, const int timeout,
